501b21ace67711261423bacec23065b8d7d6d220
dssafford <dssafford@gmail.com>
2025-05-17 10:12:54 -0500

    harpoon cd change working directory

--- lua/kickstart/plugins/harpoon.lua
+++ lua/kickstart/plugins/harpoon.lua
@@ -3,6 +3,7 @@ return {
   branch = 'harpoon2',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim', -- Optional: Telescope integration
   },
   lazy = false,
   keys = {
@@ -14,8 +15,20 @@ return {
     { '<leader>h4', function() require('harpoon'):list():select(4) end, desc = 'Harpoon file 4' },
     { '<leader>hp', function() require('harpoon'):list():prev() end, desc = 'Harpoon prev file' },
     { '<leader>hn', function() require('harpoon'):list():next() end, desc = 'Harpoon next file' },
+    { '<leader>hm', "<cmd>Telescope harpoon marks<CR>", desc = "Telescope Harpoon Marks" },
   },
-  config = function()
-    require('harpoon'):setup()
-  end,
+config = function()
+  local harpoon = require("harpoon")
+
+  harpoon:setup() -- Always call this, even if you're not passing options
+
+  -- Configure the default list's persistence settings
+  local list = harpoon:list()
+  list.config.save_on_toggle = true
+  list.config.save_on_change = true
+  list.config.sync_on_ui_close = true
+
+  -- Optional: Telescope integration
+  pcall(require("telescope").load_extension, "harpoon")
+end,
 }
--- lua/kickstart/plugins/nvim-tree.lua
+++ lua/kickstart/plugins/nvim-tree.lua
@@ -1,13 +1,13 @@
 return {
   {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
     lazy = false,
     dependencies = {
-      'nvim-tree/nvim-web-devicons',
+      "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require("nvim-tree").setup {
+      require("nvim-tree").setup({
         view = {
           width = 35,
           relativenumber = true,
@@ -31,10 +31,51 @@ return {
           enable = true,
           ignore = false,
         },
-      }
+        sync_root_with_cwd = true,
+        respect_buf_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_cwd = true,
+          update_root = true,
+        },
+      })
 
       -- Keymap to toggle the tree
       vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle File Explorer' })
+
+      -- C = visually change tree root only (no cwd change)
+      vim.keymap.set("n", "C", function()
+        local api = require("nvim-tree.api")
+        local node = api.tree.get_node_under_cursor()
+        if node and node.absolute_path then
+          api.tree.change_root(node.absolute_path)
+          print("📂 Changed tree root (cwd not updated)")
+        else
+          print("⚠️ Could not get node under cursor.")
+        end
+      end, { desc = "Change tree root only", noremap = true, silent = true })
+
+      -- Optional: manually change cwd from current file
+ -- <leader>cd = Set CWD to selected directory in tree
+vim.keymap.set("n", "<leader>cd", function()
+  local api = require("nvim-tree.api")
+  local node = api.tree.get_node_under_cursor()
+
+  if not node or not node.absolute_path then
+    print("⚠️ Not a valid node under cursor")
+    return
+  end
+
+  local path = node.absolute_path
+
+  -- If cursor is on a file, go to its parent
+  if vim.fn.isdirectory(path) == 0 then
+    path = vim.fn.fnamemodify(path, ":h")
+  end
+
+  vim.cmd("cd " .. vim.fn.fnameescape(path))
+  print("📁 Working directory set to: " .. path)
+end, { desc = "Set CWD to tree node", noremap = true, silent = true })
     end,
   },
 }
